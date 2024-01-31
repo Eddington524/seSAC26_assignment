c164413fa895a12d72cda9ec21652a41dfd7d24b
eddington524 <iamsam8687@gmail.com>
2024-01-31 22:23:54 +0900

    refactor: dispatch그룹 적용

--- seSAC26_assignment/View/ViewController.swift
+++ seSAC26_assignment/View/ViewController.swift
@@ -4,11 +4,6 @@
 //
 //  Created by Sammy Jung on 1/30/24.
 //
-//TODO://- 하나의 뷰컨트롤러에 컬렉션 1개를 추가하고, 컬렉션뷰를 섹션 (UICollectionReusableView)을 나눠 API 3개 호출해보기
-//- eX. 컬렉션뷰 0번 섹션 : TrendAPI , 1번 섹션: Top Rated API, 2번 섹션:
-//Popular API
-
-
 
 import UIKit
 import SnapKit
@@ -19,36 +14,37 @@ class ViewController: UIViewController {
     let titleList:[String] = ["뜨거운 핫 트렌드!", "평점 높은 TV!", "이보다 더 유명할 수 없는 프로그램"]
     let tableView  = UITableView()
     
-    var tvList:[[TV]] = [[],[],[]] {
-        didSet{
-            tableView.reloadData()
-        }
-    }
+    var tvList:[[TV]] = [[],[],[]]
     
    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: ViewController.configureCollectionView())
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureHierachy()
         configureView()
         setConstraintsView()
+        let group = DispatchGroup()
         
+        group.enter()
         APImanager.shared.fetchTVImages(url: "trending/tv/week") { tvs in
             self.tvList[0] = tvs
-            self.collectionView.reloadData()
+            group.leave()
         }
-        
+        group.enter()
         APImanager.shared.fetchTVImages(url: "tv/top_rated") { tvs in
-            print(tvs)
             self.tvList[1] = tvs
             self.collectionView.reloadData()
-//            self.tableView.reloadData()
+            group.leave()
         }
-        
+        group.enter()
         APImanager.shared.fetchTVImages(url: "tv/popular") { tvs in
             self.tvList[2] = tvs
-//            self.collectionView.reloadData()
-//            self.tableView.reloadData()
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            self.collectionView.reloadData()
+            self.tableView.reloadData()
         }
     }
 
@@ -81,7 +77,7 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.rowHeight = 280
         
-        tableView.register(MovieTableViewCell.self, forCellReuseIdentifier: "MovieTableViewCell")
+        tableView.register(TvTableViewCell.self, forCellReuseIdentifier: "MovieTableViewCell")
         
     }
     
@@ -137,7 +133,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath) as! MovieTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath) as! TvTableViewCell
         
         // 아하! cell 안에서도 연결 시켜줘야지!
         cell.collectionView.delegate = self
